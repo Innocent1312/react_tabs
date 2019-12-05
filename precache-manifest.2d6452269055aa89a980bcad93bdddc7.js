7dbb39fcfd88c9f5f2b4f69c206d741a5f93a2db
Innocent1312 <anthonydev33@gmail.com>
2019-12-05 22:46:27 +0200

    Updates

---
+++ precache-manifest.2d6452269055aa89a980bcad93bdddc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482ddda1f6a0734f3e2b6d372b348b9d",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "8cc4b0a3ba4f7fdc55b3",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "8d48699d83a7fbea3efd",
+    "url": "/react_tabs/static/js/2.82940fb9.chunk.js"
+  },
+  {
+    "revision": "8cc4b0a3ba4f7fdc55b3",
+    "url": "/react_tabs/static/js/main.3861178a.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
